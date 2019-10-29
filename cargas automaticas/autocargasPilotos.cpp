0496bb5c49331d190c79c633725c272ea7511996
locknet11 <52685437+locknet11@users.noreply.github.com>
2019-10-29 17:56:20 -0300

    Add files via upload

---
+++ cargas automaticas/autocargasPilotos.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+string autocargaPilotos(string Pilotos[15]){
+	Pilotos[0]="Hamilton";
+	Pilotos[1]="Verstappen";
+	Pilotos[2]="Vettel";
+	Pilotos[3]="Leclerc";
+	Pilotos[4]="Sainz";
+	Pilotos[5]="Gasly";
+	Pilotos[6]="Raikkonen";
+	Pilotos[7]="Bottas";
+	Pilotos[8]="Norris";
+	Pilotos[9]="Ricciardo";
+	Pilotos[10]="Russel";
+	Pilotos[11]="Albon";
+	Pilotos[12]="Perez";
+	Pilotos[13]="Hulkenberg";
+	Pilotos[14]="Magnussen";
+}
---
+++ cargas automaticas/autocargasPuntos.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void autocargaPuntos(int Puntos[6]){
+		Puntos[0]=25;
+		Puntos[1]=18;
+		Puntos[2]=15;
+		Puntos[3]=12;
+		Puntos[4]=10;
+		Puntos[5]=4;
+}
